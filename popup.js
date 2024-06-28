af15b57f5f90b2a8ca1191fd985aeac1338ca859
Atef Ben Ali <atef.bettaib@gmail.com>
2024-06-28 14:39:17 +0100

    refactor(popup): review `logic`

--- popup.js
+++ popup.js
@@ -7,56 +7,63 @@ document
     const statusElement = document.getElementById("status");
 
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      const url = tabs[0].url;
-      const bookmark = `- [${title}](${url})\n`;
+      try {
+        const url = tabs[0].url;
+        const bookmark = `- [ ] [${title}](${url})\n`;
 
-      chrome.storage.sync.get(["token", "repo", "path", "owner"], (data) => {
-        const { token, repo, path, owner } = data;
+        chrome.storage.sync.get(["token", "repo", "path", "owner"], (data) => {
+          const { token, repo, path, owner } = data;
 
-        fetch(
-          `https://api.github.com/repos/${owner}/${repo}/contents/${path}`,
-          {
-            method: "GET",
-            headers: {
-              Authorization: `token ${token}`,
-              Accept: "application/vnd.github.v3.raw",
-            },
-          }
-        )
-          .then((response) => response.text())
-          .then((content) => {
-            const updatedContent = content + bookmark;
-            const sha = btoa(updatedContent);
+          fetch(
+            `https://api.github.com/repos/${owner}/${repo}/contents/${path}`,
+            {
+              method: "GET",
+              headers: {
+                Authorization: `Bearer ${token}`,
+                Accept: "application/vnd.github+json",
+                "X-GitHub-Api-Version": "2022-11-28",
+              },
+            }
+          )
+            .then((response) => response.json())
+            .then((content) => {
+              const fileContent = atob(content.content);
+              const updatedContent = fileContent + bookmark;
+              const sha = content.sha;
 
-            fetch(
-              `https://api.github.com/repos/${owner}/${repo}/contents/${path}`,
-              {
-                method: "PUT",
-                headers: {
-                  Authorization: `token ${token}`,
-                  "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                  message: `Added bookmark: ${title}`,
-                  content: btoa(updatedContent),
-                  sha: sha,
-                }),
-              }
-            )
-              .then((response) => response.json())
-              .then((data) => {
-                statusElement.textContent = "Bookmark added successfully!";
-                document.getElementById("title").value = "";
-              })
-              .catch((error) => {
-                statusElement.textContent = "Error adding bookmark.";
-                console.error("Error:", error);
-              });
-          })
-          .catch((error) => {
-            statusElement.textContent = "Error fetching file.";
-            console.error("Error:", error);
-          });
-      });
+              fetch(
+                `https://api.github.com/repos/${owner}/${repo}/contents/${path}`,
+                {
+                  method: "PUT",
+                  headers: {
+                    Authorization: `Bearer ${token}`,
+                    "Content-Type": "application/json",
+                    "X-GitHub-Api-Version": "2022-11-28",
+                  },
+                  body: JSON.stringify({
+                    message: "feat: add bookmark",
+                    content: btoa(updatedContent),
+                    sha,
+                  }),
+                }
+              )
+                .then((response) => response.json())
+                .then((_) => {
+                  statusElement.textContent = "Bookmark added successfully!";
+                  document.getElementById("title").value = "";
+                })
+                .catch((error) => {
+                  statusElement.textContent = "Error adding bookmark.";
+                  console.error("Error:", error);
+                });
+            })
+            .catch((error) => {
+              statusElement.textContent = "Error fetching file.";
+              console.error("Error:", error);
+            });
+        });
+      } catch (error) {
+        console.log(`BookmarkIt | Error: ${error.message}`);
+      }
     });
   });
